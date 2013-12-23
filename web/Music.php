d3b904797180dfc3b4c43b46a53b69a160168d1c
Juha Hirvasniemi <hirvasniemi@suomi24.fi>
2013-12-23 22:38:24 +0200

    Added PHP file stubs

---
+++ web/Music.php
@@ -0,0 +1,14 @@
+<?php
+
+class Music {
+    private $artist
+
+    public function __construct($artist) {
+        //parent::__construct(something);
+        $this->artist = $artist;
+    }
+
+    //public function something() {
+    //}
+}
+?>
