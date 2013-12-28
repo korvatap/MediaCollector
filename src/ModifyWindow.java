e32b6071f03d44ddca6c7fe8f604f4643e00c1f0
Tapio Korvala <korvatap@gmail.com>
2013-12-28 04:46:35 +0200

    Created ModifyWindow.java to be implemented...

---
+++ src/ModifyWindow.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+import javax.swing.JFrame;
+
+
+public class ModifyWindow extends JFrame {
+
+	private static final long serialVersionUID = 3781413385233728385L;
+
+	public ModifyWindow(MainWindow mainRef, List<Integer> idArray) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
