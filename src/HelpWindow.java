f4561afbec40d1300aff72e6609d5ab1f2475c8b
Juha Hirvasniemi <hirvasniemi@suomi24.fi>
2013-12-30 16:47:31 +0200

    Fixes to manual texts

--- src/HelpWindow.java
+++ src/HelpWindow.java
@@ -39,13 +39,13 @@ public class HelpWindow extends JFrame {
 			case 1: helpText.setText("<html><h2>Add media instructions</h2>" +
 					"<br> - Media can be added using the Add button in the main window." +
 					"<br> - After this a window will open and there you have to fill the fields with information you want. <br>" +
-					" - The next button in the add window will create new row to the table which enables you to fastly add<br> multiple medias in to the database." +
+					" - The next button in the add window will create new row to the table which enables you to add<br> multiple medias in a fast way in to the database." +
 					"<br>- The Add button in the Add window finally then adds the new medias in to the database.<br> - This is supposed to be pressed when the information for the media has been inserted in to the table." +
 					"<br>- The cancel button just cancels the new media adding feature and no information is saved to the database. </html>");				
 			break;
 			case 2: helpText.setText("<html><h2>Media search instructions</h2><br>- If you want to search media you just need to select the correct tab in the main window first.<br>" +
 					"- After this you type to the search box the search string you want to use and press search.<br>" +
-					"- The program will then list all media find using the search string.</html>");
+					"- The program will then list all media found using the search string.</html>");
 									
 			break;
 			case 3: helpText.setText("<html><h2>How to delete media </h2<br>- You can delete media by ticking the select column of the corresponding media you want to delete.<br>" +
@@ -53,11 +53,11 @@ public class HelpWindow extends JFrame {
 									
 			break;
 			case 4: helpText.setText("<html><h2>How to modify media</h2> <br>- To be able to modify media you first have to select the media you want to modify." +
-					"<br>.- This can be achieved by ticking the select column of the target media to be modified.<br>" +
+					"<br>- This can be achieved by ticking the select column of the target media to be modified.<br>" +
 					"- After this you just press the modify button and a new window will open.<br>" +
 					"- In the new window you have a table with the media information you selected.<br>" +
 					"- Now you can modify the media information straight from the table and after that pressing save to save the media.<br>" +
-					"- Cancel button is to cancel the procedure and now modifications to the media will be saved.</html>");
+					"- Cancel button is to cancel the procedure and no modifications to the media will be saved.</html>");
 									
 			break;
 			default: helpText.setText("<html><h2>How to delete media </h2<br>- You can delete media by ticking the select column of the corresponding media you want to delete.<br>" +
@@ -76,7 +76,7 @@ public class HelpWindow extends JFrame {
 		 getContentPane().add(panel);
 		 panel.setLayout(new BorderLayout());
 		 
-		 helpText.setText("Choose which instruction you want to watch by pressing the corresponding button.");
+		 helpText.setText("Choose which instruction you want to read by pressing the corresponding button.");
 		 
 		 
 		 JButton mediaAddHelp = new JButton("How to add media");
