f73b7068d97117d8b2f38b87a6685d56f93d8ecd
Tapio Korvala <korvatap@gmail.com>
2013-12-30 03:42:24 +0200

    FileManager.java Commented

--- src/FileManager.java
+++ src/FileManager.java
@@ -5,7 +5,13 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-
+/**
+ * FileManager class store database to a file and to read it from the file.
+ * 
+ * @author      Juha Hirvasniemi <jhirvasn@paju.oulu.fi>, Tapio Korvala <korvatap@paju.oulu.fi>
+ * @version     1.0
+ * @since       2013-12-30
+ */
 public class FileManager {
 	private FileOutputStream saveFile;
 	private FileInputStream readSaveFile;
@@ -13,6 +19,9 @@ public class FileManager {
 	private ObjectOutputStream save;
 	private File dbFile;
 	
+	/**
+	 * Constructor for class FileManager.
+	 */
 	FileManager() {
 		try {
 			dbFile = new File("SaveObj.sav");
@@ -31,6 +40,11 @@ public class FileManager {
 		}
 	}
 	
+	/**
+	 * Used to read the file and get a MediaObject from it.
+	 * 
+	 * @return MediaObject gotten from file or null if does not exist.
+	 */
 	public MediaObject readFile() {
 		//File dbFile = new File("SaveObj.sav");
 		try {
@@ -62,6 +76,12 @@ public class FileManager {
 		return null;
 	}
 	
+	/**
+	 * Used to write MediaObject to file which
+	 * is usually a database of MediaItems.
+	 * 
+	 * @param db target MediaObject to be written.
+	 */
 	public void writeToFile(MediaObject db) {
 		//File dbFile = new File("SaveObj.sav");
 		try {
