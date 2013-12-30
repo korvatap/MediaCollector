614d2903ff374bead87c29e4f676e96be5bafa07
Juha Hirvasniemi <hirvasniemi@suomi24.fi>
2013-12-30 14:26:20 +0200

    Commented Music.php

--- web/Music.php
+++ web/Music.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Music is a subclass of Media. The objects of Music contain information about a song.
+ * 
+ * @author      Juha Hirvasniemi <jhirvasn@paju.oulu.fi>, Tapio Korvala <korvatap@paju.oulu.fi>
+ * @version     1.0
+ * @since       2013-12-30
+ */
 class Music extends Media {
 
     private $artist;
@@ -12,7 +19,16 @@ class Music extends Media {
             call_user_func_array (array ($this, $function), $arguments);
         } 
     }
-    
+ 
+	/**
+	 * Constructor for class Music which can store the information about a song.
+     *
+	 * @param title	        title for the song.
+	 * @param artist        name of the artist.
+	 * @param publishYear   the year the song was produced.
+	 * @param rating        the rating of the song, 1-10.
+	 * @param genre         the genre for the album e.g. Rock.
+	 */
     public function __construct5($title, $artist, $publishYear, $rating, $genre) {
         parent::setTitle($title);
         $this->artist = $artist;
@@ -23,18 +39,36 @@ class Music extends Media {
         $this->id = parent::getId();
     }
 
+	/**
+	 * Sets the name of the artist.
+	 * 
+	 * @param artist    name to be set.
+	 */
     public function setArtist($artist) {
         $this->artist = $artist;
     }
     
+	/**
+	 * Returns the artist name.
+	 * 
+	 * @return      name of the artist.
+	 */
     public function getArtist() {
         return $this->artist;
     }
     
+	/**
+	 * Returns the id of the music object.
+	 * 
+	 * @return      id of the music object.
+	 */
     public function id() {
         return $this->id;
     }
     
+	/**
+	 * Prints the info of the Music object.
+	 */
     public function info() {
         echo "-------------<br/>";
         echo "ID: " .$this->id() .", Title: " .parent::getTitle() .", Artist: " .$this->getArtist() .", PublishYear: " .parent::getPublishYear() .", Rating: " .parent::getRating() .", Genre: " .parent::getGenre() ."<br/>";
