6c240decc983775d5fb552b9b4ff4bea7433859b
Juha Hirvasniemi <hirvasniemi@suomi24.fi>
2013-12-30 10:44:51 +0200

    Commented MediaObject.php

--- web/MediaObject.php
+++ web/MediaObject.php
@@ -1,10 +1,23 @@
 <?php
 
+/**
+ * MediaObject class is used to hold MediaItems and is used as a composite class in composite pattern.
+ * 
+ * @author      Juha Hirvasniemi <jhirvasn@paju.oulu.fi>, Tapio Korvala <korvatap@paju.oulu.fi>
+ * @version     1.0
+ * @since       2013-12-30
+ */
+ 
 class MediaObject implements MediaItem {
     
     private $title;
     private $items = array();
 
+    /**
+	 * Adds new MediaItems to an array.
+	 * 
+	 * @param item	MediaItem to be added.
+	 */
     public function add($item) {
         $this->items[$item->id()] = $item;
     }
@@ -15,6 +28,11 @@ class MediaObject implements MediaItem {
         }
     }*/
     
+    /**
+	 * Removes MediaItems from the array.
+	 * 
+	 * @param item MediaItem to be removed.
+	 */
     public function remove($item) {
         unset($this->items[$item->id()]);
     }
@@ -26,20 +44,41 @@ class MediaObject implements MediaItem {
         }
     }
     
+    /**
+	 * Gets child of mediaObject.
+	 * 
+	 * @param i	Index of the item to get.
+	 * @return	MediaItem which is part of the array of MediaItems.
+	 */
     public function getChild($i) {
         if (array_key_exists ($i, $this->items) ) {
             return $this->items[$i];
         }
     }
     
+    /**
+	 * Returns the title of the mediaObject.
+	 * 
+	 * @return  title of the mediaObject.
+	 */
     public function getTitle() {
         return $this->title;
     }
     
+    /**
+	 * Sets the title of the mediaObject.
+	 * 
+	 * @param title name of the title to be set.
+	 */
     public function setTitle($title) {
         $this->title = $title;
     }
     
+    /**
+	 * Checks if array of MediaItems is empty.
+	 * 
+	 * @return	true if empty, false if not.
+	 */
     public function isEmpty() {
         return empty($this->items);
     }
