719ef1df3159206cbbba8f0b5897f1c2aef95a3e
Juha Hirvasniemi <hirvasniemi@suomi24.fi>
2013-12-30 14:27:47 +0200

    Commented MediaItem.php

--- web/MediaItem.php
+++ web/MediaItem.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * MediaItem class is used as a component in composite pattern.
+ * Contains skeleton functions for other classes that inherit MediaItem.
+ * 
+ * @author      Juha Hirvasniemi <jhirvasn@paju.oulu.fi>, Tapio Korvala <korvatap@paju.oulu.fi>
+ * @version     1.0
+ * @since       2013-12-30
+ */
 interface MediaItem {
 
     public function info();
